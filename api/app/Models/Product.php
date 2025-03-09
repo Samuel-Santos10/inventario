25a604fdb769b5b4bbcb419834d0e4b7c2d6096e
Carlos Fernández <saymonlenny@gmail.com>
2025-03-09 16:14:46 -0600

    parte del backend terminada

---
+++ api/app/Models/Product.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    //campos que seran llenados
+    protected $fillable = ['name', 'description', 'price', 'stock'];
+
+    
+
+
+}
